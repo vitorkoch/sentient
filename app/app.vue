fbb83d9f6af9dd9ff85e74719aa0eb4cb7c3d5f0
Vitor Koch <vitorgabrielkoch@gmail.com>
2025-12-27 18:08:45 -0300

    add i18n

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import { pt_br } from '#ui/locale'
+
+const title = 'Sentient'
+const description = 'Sentient is a free and open source translation management system.'
+
+useHead({
+	htmlAttrs: {
+		lang: 'pt-br',
+	},
+})
+
+useSeoMeta({
+	title,
+	description,
+})
+</script>
+
 <template>
-	<UApp>
+	<UApp :locale="pt_br">
 		<NuxtRouteAnnouncer />
 
 		<NuxtLayout>

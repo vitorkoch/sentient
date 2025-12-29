dc7fd9d7de5cb9e57beabc36eaa90e937573083d
Vitor Koch <vitorgabrielkoch@gmail.com>
2025-12-29 13:03:59 -0300

    fix date format

--- app/components/EntryHistory.vue
+++ app/components/EntryHistory.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { format as dateFormat } from 'date-fns'
+import { formatDate, parseISO } from 'date-fns'
 
 const { $orpc } = useNuxtApp()
 
@@ -22,7 +22,7 @@ const { state: entriesState } = useQuery($orpc.entry.list.queryOptions())
 
 		<template v-else>
 			<UPageGrid>
-				<UPageCard v-for="entry in entriesState.data" :key="entry.id" :title="dateFormat(entry.date, 'dd/MM/yyyy')" :description="$t(`mood.${entry.mood}`)" :icon="entry.mood" />
+				<UPageCard v-for="entry in entriesState.data" :key="entry.id" :title="formatDate(parseISO(entry.date), 'dd/MM/yyyy')" :description="$t(`mood.${entry.mood}`)" :icon="entry.mood" />
 			</UPageGrid>
 		</template>
 	</div>
